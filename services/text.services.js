8bc58eecfba315560881b5add8a1671a8396d3b4
The-Hawkeye <82801422+The-Hawkeye@users.noreply.github.com>
2024-02-26 20:22:16 +0530

    4

--- services/text.services.js
+++ services/text.services.js
@@ -1,6 +1,7 @@
 const { translate } = require('free-translate');
 
 const translateString = async (text) => {
+    console.log(text,"text from service");
     const data = await translate(text, { to: "fr" });
     return data;
 }
